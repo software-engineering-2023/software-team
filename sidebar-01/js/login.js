98d72ef2edc6226716ba7587ff2c2bfd8f9f5814
Elshouny <elshouny7@gmail.com>
2023-05-28 20:04:26 +0300

    hardcoded login code fr each stakeholder

--- sidebar-01/js/login.js
+++ sidebar-01/js/login.js
@@ -7,7 +7,27 @@ function login(event) {
   // Check the hardcoded username and password
   if (username === "admin" && password === "password") {
     // Successful login, redirect to the home page
-    window.location.href = "home.html";
+    window.location.href = "HomeAdmin.html";
+  } else if (
+    (username === "engy Client" ||
+      username === "shouny Client" ||
+      username === "khalid Client" ||
+      username === "sara Client" ||
+      username === "lama Client") &&
+    password === "password"
+  ) {
+    // Successful login, redirect to the home page
+    window.location.href = "HomeCient.html";
+  } else if (
+    (username === "engy Banker" ||
+      username === "shouny Banker" ||
+      username === "khalid Banker" ||
+      username === "sara Banker" ||
+      username === "lama Banker") &&
+    password === "password"
+  ) {
+    // Successful login, redirect to the home page
+    window.location.href = "HomeBanker.html";
   } else {
     // Incorrect username or password, display an error message
     document.getElementById("login-error").textContent =
